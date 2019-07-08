dc9f44ef6d63d513f2cd33dd4bce83d373efaf0a
JackSnowdon <jacksnowdondrums@gmail.com>
2019-07-08 13:46:48 +0000

    wired up cart page to display contents

--- cart/contexts.py
+++ cart/contexts.py
@@ -18,9 +18,9 @@ def cart_contents(request):
         created
         """
         
-        product = get_object_or_404(Tshirt, pk=id) 
-        total += quantity * product.price
+        shirt = get_object_or_404(Tshirt, pk=id) 
+        total += quantity * shirt.price
         product_count += quantity
-        cart_items.append({'id':id, 'quantity':quantity, 'product':product })
+        cart_items.append({'id':id, 'quantity':quantity, 'shirt':shirt })
         
     return { 'cart_items': cart_items, 'total': total, 'product_count': product_count }
