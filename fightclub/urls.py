eb9d4bae479194b3e19e1a661c97829992ad116f
JackSnowdon <jacksnowdondrums@gmail.com>
2019-07-11 12:13:12 +0000

    designed templated for checkout

--- fightclub/urls.py
+++ fightclub/urls.py
@@ -21,6 +21,7 @@ from home.views import index
 from shop import urls as urls_shop
 from cart import urls as urls_cart
 from search import urls as urls_search
+from checkout import urls as urls_checkout
 from django.views import static
 from .settings import MEDIA_ROOT
 
@@ -32,5 +33,6 @@ urlpatterns = [
     url(r'^shop/', include(urls_shop)),
     url(r'^cart/', include(urls_cart)),
     url(r'^search/', include(urls_search)),
+    url(r'^checkout/', include(urls_checkout)),
     url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT}),
 ]
