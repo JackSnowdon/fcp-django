995f2f16aae06d8a2df2f902db5c05857b58509b
JackSnowdon <jacksnowdondrums@gmail.com>
2019-07-11 08:36:11 +0000

    rebooted database

--- shop/models.py
+++ shop/models.py
@@ -13,7 +13,6 @@ class Tshirt(models.Model):
     name = models.CharField(max_length=254, default='')
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    shirt_size = models.CharField(max_length=2, choices=SHIRT_SIZES)
     image = models.ImageField(upload_to='images')
     
     def __str__(self):
