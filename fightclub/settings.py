07ddd94cd88087b297afcc6642a7a9ed967872b4
JackSnowdon <jacksnowdondrums@gmail.com>
2019-07-15 14:40:38 +0000

    Redo on previous commit

--- fightclub/settings.py
+++ fightclub/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-if "HOSTNAME" is os.environ:
+if "HOSTNAME":
     SECRET_KEY = os.environ.get("SECRET_KEY")
     ALLOWED_HOSTS = [os.getenv('HOSTNAME')]
     DEBUG = False
