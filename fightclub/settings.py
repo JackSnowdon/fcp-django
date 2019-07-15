4fceeec56ddf80e33d753c6fa1a83270cc359618
JackSnowdon <jacksnowdondrums@gmail.com>
2019-07-15 11:45:25 +0000

    updated allowed hosts

--- fightclub/settings.py
+++ fightclub/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-import env
+# import env
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = True
 
 HOSTNAME="b74583736384460988484c457f8fc8dc.vfs.cloud9.us-east-1.amazonaws.com"
-ALLOWED_HOSTS = [HOSTNAME]
+ALLOWED_HOSTS = [HOSTNAME, "https://fight-club-pro.herokuapp.com/"]
 
 
 # Application definition
