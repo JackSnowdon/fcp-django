e52801bf76e7972167e8c7118214019382bc97c5
JackSnowdon <jacksnowdondrums@gmail.com>
2019-07-15 11:21:23 +0000

    re done settings for travis

--- fightclub/settings.py
+++ fightclub/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-import env
+# import env
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -27,8 +27,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-
-ALLOWED_HOSTS = [env.HOSTNAME]
+HOSTNAME="b74583736384460988484c457f8fc8dc.vfs.cloud9.us-east-1.amazonaws.com"
+ALLOWED_HOSTS = [HOSTNAME]
 
 
 # Application definition
@@ -86,14 +86,19 @@ WSGI_APPLICATION = 'fightclub.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
 
-DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL')) }
+
+if "DATABASE_URL" is os.environ:
+    DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL')) }
+else:
+    ("Progres URL not found, using SQLite instead")
+    DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+    
 
 
 # Password validation
