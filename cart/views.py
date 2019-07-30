70d975f4b00fdb020364768bcbdfcc2e1875c3c3
JackSnowdon <jacksnowdondrums@gmail.com>
2019-07-30 12:33:50 +0000

    Week away from code, push to start fresh

--- cart/views.py
+++ cart/views.py
@@ -13,8 +13,6 @@ def add_to_cart(request, id):
     """Add a quantity of the specified product to the cart"""
     quantity = 1
     cart = request.session.get('cart', {})
-    size = request.POST.get('size')
-    print(size)
     if id in cart:
         cart[id] = int(cart[id]) + quantity 
     else:
