7654cbb8188cd0d71f54a1800ca04388491b6c8a
JackSnowdon <jacksnowdondrums@gmail.com>
2019-09-10 22:45:31 +0000

    Profile styling fixes, added form to bottom of training, still needs to be wired up

--- training/forms.py
+++ training/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 class ContactForm(forms.Form):
     name = forms.CharField(max_length=100)
-    sender = forms.EmailField()
+    email = forms.EmailField()
     message = forms.CharField(widget=forms.Textarea)
     phone_number = forms.IntegerField()
--- training/views.py
+++ training/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .forms import *
 
 # Create your views here.
 def training(request):
     """ Redeners traning """
-    return render(request, 'training.html')
+    contact_form = ContactForm()
+    return render(request, 'training.html', {'contact_form': contact_form})
